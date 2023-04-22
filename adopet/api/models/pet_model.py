c1c27f4caf50779f686eb823ff69638096536ac4
derek.coelho <derek.coelho.ext@vr.com.br>
2023-04-22 00:09:42 -0300

    feat: created model PetModel

---
+++ adopet/api/models/pet_model.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, types, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship, mapped_column
+from sqlalchemy_utils import URLType
+
+from api.models.animal_specie_model import AnimalSpecieModel
+from api.models.base_model import StandardModelMixin
+from api.models.city_model import CityModel
+from api.models.size_model import SizeModel
+from api.models.state_model import StateModel
+from api.models.status_model import StatusModel
+from api.models.user_model import UserModel
+
+BASE = declarative_base(cls=StandardModelMixin)
+
+
+class PetModel(BASE):
+    __tablename__ = 'pet'
+    photo_url = Column(URLType, nullable=True)
+    name = Column(types.String(155), nullable=False)
+    personality = Column(types.String, nullable=False)
+
+    owner_id = mapped_column(types.UUID, ForeignKey(UserModel.id, ondelete="CASCADE"))
+    specie_id = mapped_column(types.UUID, ForeignKey(AnimalSpecieModel.id, ondelete="CASCADE"))
+    status_id = mapped_column(types.UUID, ForeignKey(StatusModel.id, ondelete="CASCADE"))
+    size_id = mapped_column(types.UUID, ForeignKey(SizeModel.id, ondelete="CASCADE"))
+    state_id = mapped_column(types.UUID, ForeignKey(StateModel.id, ondelete="CASCADE"))
+    city_id = mapped_column(types.UUID, ForeignKey(CityModel.id, ondelete="CASCADE"))
+
+    owner = relationship("UserModel", back_populates="pets")

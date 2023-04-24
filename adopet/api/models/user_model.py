83d579e7613130167dd36f294fd33e4706c044e1
derek.coelho <derek.coelho.ext@vr.com.br>
2023-04-24 18:22:06 -0300

    refactor: changed column type

--- adopet/api/models/user_model.py
+++ adopet/api/models/user_model.py
@@ -2,9 +2,7 @@ from api.models.base_model import StandardModelMixin
 from sqlalchemy import Column
 from sqlalchemy import types
 from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import relationship
 from sqlalchemy_utils import EmailType
-from sqlalchemy_utils import PasswordType
 from sqlalchemy_utils import URLType
 
 BASE = declarative_base(cls=StandardModelMixin)
@@ -16,19 +14,9 @@ class UserModel(BASE):
     email = Column(EmailType, nullable=False, index=True, unique=True)
     phone_number = Column(types.String, nullable=False)
     password = Column(
-        PasswordType(
-            schemes=["pbkdf2_sha512", "md5_crypt"],
-            deprecated=["md5_crypt"],
-            max_length=8,
-        ),
+        types.String,
         unique=True,
     )
     role = Column(types.String, nullable=False)
     about = Column(types.String, nullable=False)
     photo_url = Column(URLType, nullable=True)
-    pets = relationship(
-        "PetModel",
-        back_populates="owner",
-        cascade="all, delete",
-        passive_deletes=True,
-    )

52e6b5a6ee6da6442ffe983dd4dac32d0403f2af
derek.coelho <derek.coelho.ext@vr.com.br>
2023-04-24 18:22:32 -0300

    feat: included new param to enum column type

--- adopet/api/models/state_model.py
+++ adopet/api/models/state_model.py
@@ -1,7 +1,8 @@
 from api.enums.state_enum import StateEnum
 from api.models.base_model import StandardModelMixin
+from core.config import settings
 from sqlalchemy import Column
-from sqlalchemy import types
+from sqlalchemy import Enum
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -12,10 +13,11 @@ class StateModel(BASE):
     __tablename__ = "state"
 
     name = Column(
-        types.Enum(
+        Enum(
             StateEnum,
             values_callable=lambda obj: [e.value for e in obj],
             create_constraint=True,
+            schema=settings.DATABASE_SCHEMA,
         ),
         nullable=False,
     )

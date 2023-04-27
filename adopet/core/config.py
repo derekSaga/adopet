6a3188f73abe9ac1dc026905433d0049e9f88e40
derek.coelho <derek.coelho.ext@vr.com.br>
2023-04-27 11:47:01 -0300

    env: included new variable environment

--- adopet/core/config.py
+++ adopet/core/config.py
@@ -1,12 +1,11 @@
 import os
 
 from pydantic import BaseSettings
-from sqlalchemy import MetaData
-from sqlalchemy.orm import declarative_base
 
 
 class Settings(BaseSettings):
-    API_V1_STR = "/api/v1"
+    API_V1_STR: str = "/api/v1"
+    API_PORT: int = int(os.environ["API_PORT"])
 
     DB_URL: str = "%s://%s:%s@%s:%s/%s" % (
         os.environ["DRIVER_NAME"],
@@ -17,7 +16,6 @@ class Settings(BaseSettings):
         os.environ["POSTGRES_DB"],
     )
     DATABASE_SCHEMA: str = os.environ["DATABASE_SCHEMA"]
-    BASE: declarative_base = declarative_base(metadata=MetaData(schema=DATABASE_SCHEMA))
 
     ACCESS_TOKEN_EXPIRE_MINUTES = 1  # 30 minutes
     REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 1 * 7  # 7 days

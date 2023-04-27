9bc2a2778d5bac8cd460d434caaaf0dc2747cffe
derek.coelho <derek.coelho.ext@vr.com.br>
2023-04-27 12:01:39 -0300

    feat: included new middleware

--- adopet/main.py
+++ adopet/main.py
@@ -1,10 +1,19 @@
+import pathlib
+
+import yaml
 from api.v1.api import api_router
+from asgi_correlation_id import CorrelationIdMiddleware
 from core.config import settings
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
 app = FastAPI(title="Adopet")
 app.include_router(api_router, prefix=settings.API_V1_STR)
+
+app.add_middleware(
+    CorrelationIdMiddleware,
+    header_name="X-Request-ID",
+)
 
 
 @app.get("/", response_class=RedirectResponse, include_in_schema=False)
@@ -15,4 +24,15 @@ async def docs():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info", reload=True)
+    cwd = pathlib.Path(__file__).parent.resolve()
+    with open(cwd.parent.joinpath("logging.yaml"), "rt") as file:
+        config = yaml.safe_load(file.read())
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=settings.API_PORT,
+        log_level="info",
+        reload=True,
+        log_config=config,
+    )

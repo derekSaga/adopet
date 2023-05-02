9e08180a90cb4d3da40a71fbf9dce07056ca93fe
derek.coelho <derek.coelho.ext@vr.com.br>
2023-05-02 18:49:09 -0300

    rm

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import pytest
 from dotenv import load_dotenv
 
@@ -9,3 +11,11 @@ from core.config import Settings
 @pytest.fixture(scope="module")
 def fxt_settings() -> Settings:
     return Settings()
+
+
+@pytest.fixture(scope="class")
+def event_loop_instance(request):
+    """ Add the event_loop as an attribute to the unittest style test class. """
+    request.cls.event_loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield
+    request.cls.event_loop.close()
--- tests/unit/core/test_deps/test_get_session.py
+++ tests/unit/core/test_deps/test_get_session.py
@@ -5,12 +5,12 @@ from core.config import Settings
 from core.deps import get_session
 
 
+@pytest.mark.usefixtures("event_loop_instance")
 class TestGetSession:
     @pytest.mark.asyncio
     async def test_a(self, fxt_settings: Settings):
         with mock.patch(
-            "core.deps.create_async_engine", autospec=True
+                "core.deps.create_async_engine", autospec=True
         ) as fxt_create_async_engine:
             await get_session().__anext__()
-
         fxt_create_async_engine.assert_called_with(fxt_settings.DB_URL)

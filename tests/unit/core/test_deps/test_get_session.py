0686fab7375a7a2ca63f55658ca7634007c44628
derek.coelho <derek.coelho.ext@vr.com.br>
2023-05-02 18:11:23 -0300

    feat: added get session db tests

---
+++ tests/unit/core/test_deps/test_get_session.py
@@ -0,0 +1,16 @@
+from unittest import mock
+
+import pytest
+from core.config import Settings
+from core.deps import get_session
+
+
+class TestGetSession:
+    @pytest.mark.asyncio
+    async def test_a(self, fxt_settings: Settings):
+        with mock.patch(
+            "core.deps.create_async_engine", autospec=True
+        ) as fxt_create_async_engine:
+            await get_session().__anext__()
+
+        fxt_create_async_engine.assert_called_with(fxt_settings.DB_URL)

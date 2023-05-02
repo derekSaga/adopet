a7387b1555d0d65cca6a61aca333ce0e747b80e4
derek.coelho <derek.coelho.ext@vr.com.br>
2023-05-02 19:28:08 -0300

    tests: add get session test

--- tests/unit/core/test_deps/test_get_session.py
+++ tests/unit/core/test_deps/test_get_session.py
@@ -1,16 +1,13 @@
 from unittest import mock
+from unittest.mock import MagicMock
 
 import pytest
 from core.config import Settings
 from core.deps import get_session
 
 
-@pytest.mark.usefixtures("event_loop_instance")
 class TestGetSession:
     @pytest.mark.asyncio
-    async def test_a(self, fxt_settings: Settings):
-        with mock.patch(
-                "core.deps.create_async_engine", autospec=True
-        ) as fxt_create_async_engine:
-            await get_session().__anext__()
+    async def test_b(self, fxt_settings: Settings, fxt_create_async_engine: MagicMock):
+        _ = [item async for item in get_session()]
         fxt_create_async_engine.assert_called_with(fxt_settings.DB_URL)

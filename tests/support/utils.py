75450808add1a44704ed69bff892157ac63425f0
derek.coelho <derek.coelho.ext@vr.com.br>
2023-05-15 15:10:12 -0300

    refactor: modified a test support function

--- tests/support/utils.py
+++ tests/support/utils.py
@@ -1,6 +1,8 @@
+import random
 from typing import Any
 from typing import Dict
 
+from api.enums.user_role_enum import UserRoleEnum
 from api.models.repositories.user_repository import UserRepository
 from faker import Faker
 from fastapi.testclient import TestClient
@@ -14,7 +16,7 @@ def user_data_with_password() -> Dict[str, Any]:
         "email": myFactory.email(),
         "phone_number": myFactory.msisdn(),
         "password": user_password(),
-        "role": "myFactory.role()",
+        "role": random.choice(list(UserRoleEnum)).value,
         "about": myFactory.text(),
         "photo_url": myFactory.image_url(),
     }
@@ -35,10 +37,6 @@ def user_authentication_headers(client: TestClient, email: str, password: str) -
 async def authentication_token_from_email(
     client: TestClient, email: str, user_repository: UserRepository
 ) -> str:
-    """
-    Return a valid token for the user with given email.
-    If the user doesn't exist it is created first.
-    """
     password = user_password()
     user = await user_repository.get_user_by_email(email=email)
     return user_authentication_headers(

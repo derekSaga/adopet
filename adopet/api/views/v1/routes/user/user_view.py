e1ec5f783c0299ed9da3f9781e9525f6c8d42e48
derek.coelho <derek.coelho.ext@vr.com.br>
2023-05-19 13:50:57 -0300

    refactor: removed unnecessary methods from view

--- adopet/api/views/v1/routes/user/user_view.py
+++ adopet/api/views/v1/routes/user/user_view.py
@@ -1,17 +1,11 @@
 import logging
 
-from api.enums.user_role_enum import UserRoleEnum
 from api.models.repositories.user_repository import UserRepository
-from api.models.user_model import UserModel
 from api.schemas.v1.token_schema import SystemUser
 from api.schemas.v1.token_schema import TokenSchema
 from api.schemas.v1.token_schema import UserOut
-from api.schemas.v1.user_schema import UserSchemaBase
-from api.schemas.v1.user_schema import UserSchemaCreate
-from api.schemas.v1.user_schema import UserTeste
 from api.views.v1.utils.auth import create_access_token
 from api.views.v1.utils.auth import create_refresh_token
-from api.views.v1.utils.auth import get_hashed_password
 from api.views.v1.utils.auth import verify_password
 from core.deps.get_current_user import get_current_user_request
 from core.deps.get_session_db import get_session
@@ -20,29 +14,10 @@ from fastapi import Depends
 from fastapi import HTTPException
 from fastapi import status
 from fastapi.security import OAuth2PasswordRequestForm
-from fastapi_pagination import Page
 from sqlalchemy.ext.asyncio import AsyncSession
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
-
-
-@router.post(
-    "/tutores",
-    status_code=status.HTTP_201_CREATED,
-    response_model=UserSchemaBase,
-    summary="Create new tutor",
-)
-async def post_user(user: UserSchemaCreate, db: AsyncSession = Depends(get_session)):
-    logger.info("creating user".title())
-
-    new_user: UserModel = UserModel(**user.dict())
-
-    new_user.password = get_hashed_password(new_user.password)
-
-    result = UserRepository(db).create_user(new_user)
-
-    return result
 
 
 @router.post(
@@ -80,9 +55,3 @@ async def login(
 )
 async def get_me(user: SystemUser = Depends(get_current_user_request)):
     return user
-
-
-@router.get(path="/tutores", summary="List all tutors", response_model=Page[UserTeste])
-async def get_users(db: AsyncSession = Depends(get_session)):
-    users = await UserRepository(db).get_all_users(UserRoleEnum.TUTOR.value)
-    return users

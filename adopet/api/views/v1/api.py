5227e52eda5aedf51c59461987b3e6517ca9abba
derek.coelho <derek.coelho.ext@vr.com.br>
2023-05-19 13:52:01 -0300

    feat: included new route

--- adopet/api/views/v1/api.py
+++ adopet/api/views/v1/api.py
@@ -1,6 +1,8 @@
+from api.views.v1.routes.user import tutor_view
 from api.views.v1.routes.user import user_view
 from fastapi import APIRouter
 
 api_router = APIRouter()
 
 api_router.include_router(user_view.router, prefix="/user", tags=["user"])
+api_router.include_router(tutor_view.router, prefix="/tutor", tags=["tutor"])
